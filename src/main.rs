928b7a4215b0f23bcaa89ba64b332441f3f4ee29
Tim Visée <timvisee@gmail.com>
2016-11-07 14:20:44 +0100

    Create function to fetch OS'es PATH variable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate clap;
 
 use clap::App;
+use std::process::Command;
+use std::env;
 
 const APP_NAME: &'static str = "NotesToolbox";
 const APP_VERSION_NAME: &'static str = "v0.1.0-alpha";
@@ -11,12 +13,16 @@ const APP_DESCRIPTION: &'static str = "Toolbox project for compiling notes into 
         some other formats!";
 
 /// Start the application.
+#[allow(unused_variables)]
 fn main() {
     // Handle command line arguments for help
     handle_arguments();
 
     // Show placeholder message
     println!("Hello, world!");
+
+    // Get the environment PATH variable and print it to the console
+    println!("PATH: {:?}", get_env_path().unwrap());
 }
 
 /// Handle program arguments passed along with the command line to show things like help pages.
@@ -27,4 +33,33 @@ fn handle_arguments() {
         .author(APP_AUTHOR)
         .about(APP_DESCRIPTION)
         .get_matches();
+}
+
+/// Get the PATH variable from the OS environment variables.
+fn get_env_path() -> Option<String> {
+    // Iterate through the environment variables of the OS
+    for (key, value) in env::vars_os() {
+        // Get the OsString (key) as a string
+        let result = key.into_string();
+
+        // Make sure the conversion succeeded
+        if result.is_err() {
+            // TODO: Should we use panic here?
+            panic!("Failed to convert OsString into String");
+        }
+
+        // Get the Rust string
+        let key_str = result.unwrap();
+
+        // Continue the loop if this isn't the PATH variable
+        if key_str != "PATH".to_string() {
+            continue;
+        }
+
+        // Return the option with the path value
+        return Some(value.into_string().unwrap());
+    }
+
+    // Variable not found, return nothing
+    return None;
 }
