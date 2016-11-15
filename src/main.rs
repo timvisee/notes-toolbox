d42da240caa2567b27183316af177b0dfb3bc08b
Tim Visée <timvisee@gmail.com>
2016-11-15 22:03:44 +0100

    Create remove_dir_all_force function, to force remove a directory structure on Windows machines

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ extern crate tempdir;
 
 use clap::App;
 use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::Read;
+use std::io::Error;
 use std::path::Path;
+use std::path::PathBuf;
 
 const APP_NAME: &'static str = "NotesToolbox";
 const APP_VERSION_NAME: &'static str = "v0.1.0-alpha";
@@ -177,6 +180,41 @@ fn get_program_paths(dir: Option<String>) -> Vec<String> {
     paths
 }
 
+/// Force remove a directory structure (including all of it's contents).
+/// The regular `fs::remove_dir_all(...)` function might inconsistently fail on Windows machines.
+/// This function forces to delete the directory, by repeatedly invoking the remove command until
+/// the file is successfully removed. The remove command is attempted a maximum number of times to
+/// prevent locking.
+///
+/// True is returned if the directory was successfully removed. False is returned if the directory
+/// structure couldn't be removed, and the maximum number of remove attempts is reached.
+///
+/// # Example
+///
+/// Force remove a directory structure on Windows:
+/// ```no_run
+/// remove_dir_all_force(Path::new("~/myfile");
+/// ```
+pub fn remove_dir_all_force(path: PathBuf) -> bool {
+    // Create a variable to define the number of attempts we've left
+    // TODO: Don't hardcode this value in the function itself, create a constant?
+    let mut attempt: u8 = 8;
+
+    // Keep trying to remove the directory until attempts are drained
+    while attempt > 0 {
+        // Try to remove the directory, and return on success
+        if fs::remove_dir_all(&path).is_ok() {
+            return true;
+        }
+
+        // Decrease the number of attempts
+        attempt -= 1;
+    }
+
+    // We failed
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs;
@@ -188,6 +226,7 @@ mod tests {
     use super::get_env_path;
     use super::get_program_paths;
     use super::load_file_vec;
+    use super::remove_dir_all_force;
 
     #[test]
     fn get_env_path_test() {
@@ -230,9 +269,8 @@ mod tests {
         // Remove the temporarily created file
         fs::remove_file(&file_path).unwrap();
 
-        // Close the temporary directory
-        // TODO: This breaks on windows, due to a permission error
-        temp_dir.close().expect("failed to close temporary directory");
+        // Remove the temporary directory
+        remove_dir_all_force(temp_dir.into_path());
 
         // TODO: Remove debug messages
     }
