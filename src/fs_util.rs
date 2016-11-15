72675785a66d4b825115a54475fbf82b69b2870c
Tim Visée <timvisee@gmail.com>
2016-11-15 23:00:31 +0100

    Sleep between each file remove attempt

--- src/fs_util.rs
+++ src/fs_util.rs
@@ -1,5 +1,10 @@
 use std::fs;
 use std::path::PathBuf;
+use std::thread;
+use std::time::Duration;
+
+/// Maximum number of attempts to try and force remove a directory structure
+const REMOVE_DIR_FORCE_ATTEMPTS: u8 = 16;
 
 /// Force remove a directory structure (including all of it's contents).
 /// The regular `fs::remove_dir_all(...)` function might inconsistently fail on Windows machines.
@@ -23,19 +28,21 @@ pub fn remove_dir_all_force(path: PathBuf) -> bool {
         return fs::remove_dir_all(path).is_ok();
     }
 
-    // Create a variable to define the number of attempts we've left
-    // TODO: Don't hardcode this value in the function itself, create a constant?
-    let mut attempt: u8 = 8;
+    // Keep track of the attempt count
+    let mut attempt: u8 = 0;
 
     // Keep trying to remove the directory until attempts are drained
-    while attempt > 0 {
+    while attempt < REMOVE_DIR_FORCE_ATTEMPTS {
         // Try to remove the directory, and return on success
         if fs::remove_dir_all(&path).is_ok() {
             return true;
         }
 
-        // Decrease the number of attempts
-        attempt -= 1;
+        // Increase the number of attempts
+        attempt += 1;
+
+        // Sleep the thread
+        thread::sleep(Duration::new(0, 2u32.pow(attempt as u32)));
     }
 
     // We failed
