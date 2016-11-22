99140c6f34ba9c721a32d0d136d988b48e89c8ac
Tim Visée <timvisee@gmail.com>
2016-11-22 17:21:33 +0100

    Create constructors for formatter and parser classes

--- src/doc/formatter.rs
+++ src/doc/formatter.rs
@@ -5,6 +5,15 @@ pub struct Formatter {
 }
 
 impl Formatter {
+    /// Constructor.
+    ///
+    /// The document type for this formatter should be passed to the `doc_type` parameter.
+    fn new(doc_type: &'static DocType) -> Formatter {
+        Formatter {
+            doc_type: doc_type
+        }
+    }
+
     /// Get the document type this formatter is for
     fn get_type(&self) -> &'static DocType {
         self.doc_type
--- src/doc/parser.rs
+++ src/doc/parser.rs
@@ -5,6 +5,15 @@ pub struct Parser {
 }
 
 impl Parser {
+    /// Constructor.
+    ///
+    /// The document type for this parser should be passed to the `doc_type` parameter.
+    fn new(doc_type: &'static DocType) -> Parser {
+        Parser {
+            doc_type: doc_type
+        }
+    }
+
     /// Get the document type this parser is for
     fn get_type(&self) -> &'static DocType {
         self.doc_type
